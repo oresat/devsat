d6367033ac8f165841f5c324912afb44cd344113
Evan Yand <evanyand@gmail.com>
2017-08-02 13:40:44 -0700

    added more to semtech test fn

--- firmware/chib_stf4x/src/app_semtech/semtech.c
+++ firmware/chib_stf4x/src/app_semtech/semtech.c
@@ -19,6 +19,21 @@ void semtech_test_read(SPIDriver *spip){
     //Will request the values of some registers, and write the output to serial.
     chprintf(DEBUG_CHP, "\r\nTesting SPI Connection...\r\n");
 
+    //Read one byte using single access mode
+    int txData = 0b00000010;     //Address for RegBitrateMsb 0x02 in read mode (MSB is 0)
+    int rxData = 0b00000000;     //Received data buffer
+
+    spiSelect(&SPID1);
+    spiStartExchange(&SPID1, 1, &txData, &rxData);
+    
+    //Wait for exchange to complete
+    while(SPID1.state != SPI_READY){ }
+
+
+    spiUnselect(&SPID1);
+
+    chprintf(DEBUG_CHP, "\r\nValue in RegBitrateMsb is 0x%x\r\n", &rxData);
+    chprintf(DEBUG_CHP, "\r\nDefault value is 0x1a\r\n");
 
 
 }
